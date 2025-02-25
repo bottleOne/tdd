2da6d5e793b9bb10d49c32c33ef71d8c2ed33b1f
Jeon_Byeongil <ekdh523@ant-soft.co.kr>
2025-02-25 12:54:21 +0900

    상위 객체 생성 - 공용메서드 정리

---
+++ src/main/java/com/tdd/example/tdd/domain/Franc.java
@@ -0,0 +1,14 @@
+package com.tdd.example.tdd.domain;
+
+
+public class Franc extends Money{
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+    public Franc times(int multiplier){
+        return new Franc(this.amount * multiplier);
+    }
+
+
+}
